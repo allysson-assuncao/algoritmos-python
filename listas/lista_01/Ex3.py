e2b335027f55ad56e31e469e32ece68d53fdd4a5
allysson-assuncao <anybody723@proton.me>
2026-03-20 19:16:41 -0300

    finalizando o último exercício da primeira lista

--- listas/lista_01/Ex3.py
+++ listas/lista_01/Ex3.py
@@ -8,35 +8,45 @@
 #                  (2, 3)]
 
 def solucao1():
-    n = int(input("Qntd de jogadores: "))
-    jogadores = [str(input("Informe o nome dos jogadores: ")) for i in range(n)]
+    n = int(input("Total de jogadores no torneio? "))
+    jogadores = [input(f"Nome do jogador {i+1}: ") for i in range(n)]
 
+    jogos = [(jogadores[i], jogadores[j])
+                  for i in range(len(jogadores))
+                  for j in range(i + 1, len(jogadores))]
 
+    print("\nPerimeira Fase de Jogos")
+    resultados = {}
 
+    for p1, p2 in jogos:
+        print(f"\nPartida: {p1} vs {p2}")
+        vencedor = input(f"Quem venceu? ({p1}/{p2}/Empate): ")
+        resultados[(p1, p2)] = vencedor
 
-    tuplas = [(i, j) for i in range(n) for j in range(i + 1, n)]
-    for x in tuplas:
-        for y in x:
-            print(str(y) + ", ")
+    pontos = {nome: 0 for nome in jogadores}
+
+    for (p1, p2), v in resultados.items():
+        if v == p1:
+            pontos[p1] += 3
+        elif v == p2:
+            pontos[p2] += 3
+        else:
+            pontos[p1] += 1
+            pontos[p2] += 1
+
+    print("\n--- Resultado Final ---")
+    for jogador, score in sorted(pontos.items(), key=lambda x: x[1], reverse=True):
+        print(f"{jogador}: {score} pontos")
 
 def solucao2():
 
     n = int(input("Qntd de jogadores: "))
     jogadores = [str(input("Informe o nome dos jogadores(as): ")) for i in range(n)]
 
-
-
-
-
-
 def solucao3():
 
     n = int(input("Qntd de jogadores: "))
     jogadores = [str(input("Informe o nome dos jogadores(as): ")) for i in range(n)]
-
-
-
-
 
 if __name__ == '__main__':
     solucao1()
