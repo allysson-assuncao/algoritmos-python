dce57a63a66be1530078237146d827d0f9aab40d
allysson-assuncao <anybody723@proton.me>
2026-05-07 20:33:03 -0300

    feat(exercicios): formatando e testando a solução

--- exercícios_avaliados/Ex2/solucao.py
+++ exercícios_avaliados/Ex2/solucao.py
@@ -1,28 +1,32 @@
 import sys
 import time
-import numpy as np  # importa o pacote com 'alias' ou apelido de 'np'
+import numpy as np
 import math
-import matplotlib.pyplot as plt    # o módulo pyplot, parte do matplotlib, como plt
-np.random.seed(1000)    ## produz o mesmos numeros 'pseudo'aleatórios a cada execução
+import matplotlib.pyplot as plt
+
+np.random.seed(1000)
+
 
 def le_arq_vetor(arquivo):
     v = []
     with open(arquivo, "r") as arquivo:
         linhas = arquivo.readlines()
-        #print(linhas)
+        # print(linhas)
     for linha in linhas:
         l = linha.split()
         v.append((int(l[0]), int(l[1]), int(l[2])))
     return np.array(v)
 
+
 def gera_matriz_dist(data, cid):
     num_cid = cid.shape[-1]
-    dist = np.zeros((num_cid,num_cid))
+    dist = np.zeros((num_cid, num_cid))
     for i in np.arange(num_cid):
         for j in np.arange(num_cid):
-            dist[i, j] = np.sqrt(data[i][1]*data[j][1] + data[i][2]*data[j][2])
-            dist[j, i] =  dist[i, j]
+            dist[i, j] = np.sqrt(data[i][1] * data[j][1] + data[i][2] * data[j][2])
+            dist[j, i] = dist[i, j]
     return dist
+
 
 def gera_vetor_cidades(data):
     cidades = np.zeros(data.shape[0])
@@ -30,9 +34,11 @@ def gera_vetor_cidades(data):
         cidades[i] = data[i][0]
     return cidades
 
+
 data = le_arq_vetor("C50.TXT")
 cidades = gera_vetor_cidades(data)
 dist = gera_matriz_dist(data, cidades)
+
 
 # Avalia o custo de uma solução s do TSP simples (soma das distâncias)
 def fs(s):
@@ -42,7 +48,7 @@ def fs(s):
     # Soma a distância de cada cidade para a próxima no caminho
     for i in range(n - 1):
         cidade_atual = int(s[i])
-        proxima_cidade = int(s[i+1])
+        proxima_cidade = int(s[i + 1])
         custo += dist[cidade_atual, proxima_cidade]
 
     # Adiciona a distância da última cidade de volta para a primeira (origem)
@@ -52,11 +58,12 @@ def fs(s):
 
     return custo
 
+
 def constroiGuloso(n, matrizDist, fs):
     # n: numero de cidades:  de 0..n
     # matrizDist: matrix nxn de distâncias
     # fs: função de avaliação da solução
-    s = [0] # Partindo da cidade 0
+    s = [0]  # Partindo da cidade 0
     visitados = set([0])
     atual = 0
 
@@ -76,6 +83,7 @@ def constroiGuloso(n, matrizDist, fs):
 
     return s
 
+
 # Gera vizinhança de uma solução s, fazendo movimentos de troca (swap).
 def NS_swap(s, n):
     # s: a lista ou array de elementos
@@ -90,7 +98,8 @@ def NS_swap(s, n):
             vizinho[i], vizinho[j] = vizinho[j], vizinho[i]
             vizinhos.append(vizinho)
 
-    return vizinhos # retorno: lista contendo todas as soluções vizinhas
+    return vizinhos  # retorno: lista contendo todas as soluções vizinhas
+
 
 # Gera vizinhança de uma solução s, fazendo movimentos de insercao.
 def NS_insertion(s, n):
@@ -112,6 +121,7 @@ def NS_insertion(s, n):
 
     # Converte as tuplas de volta para listas antes de retornar
     return [list(v) for v in vizinhos_unicos]
+
 
 # Método da descida com Best Improvement
 def descida(s_inicial, n, funcao_vizinhanca):
@@ -137,12 +147,14 @@ def descida(s_inicial, n, funcao_vizinhanca):
         if menor_custo_vizinhanca < custo_atual:
             s_atual = melhor_vizinho
             custo_atual = menor_custo_vizinhanca
-            melhoria = True # Continua o loop procurando mais melhorias
+            melhoria = True  # Continua o loop procurando mais melhorias
 
     return s_atual, custo_atual
 
+
 def leInt(msg):
     return int(input(msg))
+
 
 def permutar(arr, i=0):
     if i == len(arr):
@@ -157,6 +169,7 @@ def permutar(arr, i=0):
 
         # desfaz a troca (backtrack)
         arr[i], arr[j] = arr[j], arr[i]
+
 
 arr = ['a', 'b', 'c', 'd']
 permutar(arr)
